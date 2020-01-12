f21353ce9ba3508c289e82ebf083dbd1f066f658
yeongbin.byeon <yeongbin6483@gmail.com>
2020-01-13 01:47:02 +0900

    3-4 commit

--- ZeroCho_vue_basic/숫자야구/NumberBaseball.vue
+++ ZeroCho_vue_basic/숫자야구/NumberBaseball.vue
@@ -2,10 +2,16 @@
     <div>
         <h1>{{result}}</h1>
         <form v-on:submit="onSubmitForm">
-            <input ref="answer" maxlength="4" v-model="value" />
-            <button>입력</button>
+            <input ref="answer" minlength="4" maxlength="4" v-model="value" />
+            <button type="submit">입력</button>
         </form>
-        <div>시도: {{}}</div>
+        <div>시도: {{tries.length}}</div>
+        <ul>
+            <li v-for="(t,index) in tries" v-bind:key="index">
+                <div>{{t.try}}</div>
+                <div>{{t.result}}</div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -13,6 +19,7 @@
 export default {
     data(){
         return{
+            tries:[],
             value:'',
             result:'',
         }
@@ -20,6 +27,12 @@ export default {
     methods:{
         onSubmitForm(e){
             e.preventDefault();
+            this.tries.push({
+                try: this.value,
+                result:'홈런',
+            });
+            this.value='';
+            this.$refs.answer.focus();
         }
     }
 };
