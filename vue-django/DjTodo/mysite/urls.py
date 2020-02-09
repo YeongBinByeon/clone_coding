e0b88975b52d250b82c7c864eef9654b789c3920
yeongbin.byeon <yeongbin6483@gmail.com>
2020-02-10 01:22:47 +0900

    cdn골격잡기까지

--- vue-django/DjTodo/mysite/urls.py
+++ vue-django/DjTodo/mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', HomeView.as_view(), name='home'),
     path('todo/', include('todo.urls')),
 ]
