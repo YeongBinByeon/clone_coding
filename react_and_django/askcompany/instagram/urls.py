58adce0d79e6ed89593c5175bbfdab9c74e0e0b2
yeongbin.byeon <yeongbin6483@gmail.com>
2020-03-16 21:22:48 +0900

    chapter3/01까지

--- react_and_django/askcompany/instagram/urls.py
+++ react_and_django/askcompany/instagram/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list),
+    path('<int:pk>/', views.post_detail),
 ]
--- react_and_django/askcompany/instagram/views.py
+++ react_and_django/askcompany/instagram/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import ListView
 
 # Create your views here.
 from .models import Post
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q','')
-    if q:
-        qs = qs.filter(message__icontains=q)
+post_list = ListView.as_view(model=Post)
+
+# def post_list(request):
+#     print(request)
+#     qs = Post.objects.all()
+#     q = request.GET.get('q','')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
     
-    # instagram/templates/instagram/post_list.html
-    return render(request, 'instagram/post_list.html', {
-        'post_list':qs,
-        'q':q,
-    })
+#     # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list':qs,
+#         'q':q,
+#     })
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    pass
+
