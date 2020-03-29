0caa2d1100cafbe9b62a953bf7b33ccad48aa54c
yeongbin.byeon <yeongbin6483@gmail.com>
2020-03-29 15:26:53 +0900

    chapter 4-06 내용 commit

--- react_and_django/askcompany/askcompany/settings.py
+++ react_and_django/askcompany/askcompany/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = '' #TODO
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askcompany', 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
