9601e58068cea315292f76921be8cec24b3e3444
yeongbin.byeon <yeongbin6483@gmail.com>
2020-06-18 02:08:01 +0900

    장고 기본 인증) 2 사용자 ㅍ 프로필 페이지 및 프로필 수정

--- react_and_django/askcompany/accounts/views.py
+++ react_and_django/askcompany/accounts/views.py
@@ -1,2 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from accounts.models import Profile
+from django.views.generic import UpdateView
+from .forms import ProfileForm
 
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
+
+@login_required
+def profile_edit(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = None
+        #Profile.objects.get(user=request.user) #위의 코드와 같은 코드이다.
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'accounts/profile_form.html', {
+        'form':form,
+    })
