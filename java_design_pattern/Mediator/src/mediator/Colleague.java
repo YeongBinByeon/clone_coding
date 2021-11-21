8a7c3273e7474340fadf8519094edfe349a632c0
yeongbin.byeon <yeongbin6483@gmail.com>
2021-11-22 01:23:09 +0900

    Mediator 패턴, 카운셀러는 하나 뿐

---
+++ java_design_pattern/Mediator/src/mediator/Colleague.java
@@ -0,0 +1,7 @@
+package mediator;
+
+public interface Colleague {
+    public abstract void setMediator(Mediator mediator);
+    public abstract void setColleagueEnabled(boolean enabled);
+}
+
---
+++ java_design_pattern/Mediator/src/mediator/Main.java
@@ -0,0 +1,9 @@
+package mediator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new LoginFrame("Mediator Sample");
+	}
+
+}
