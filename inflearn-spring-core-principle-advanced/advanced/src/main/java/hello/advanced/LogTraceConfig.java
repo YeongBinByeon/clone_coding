4b61b8c336927c5306869f2e3dc8fb1e73c8b3ba
yeongbin <yeongbin6483@gmail.com>
2022-10-02 07:28:45 +0900

    Thread local 전체 내용 커밋

--- inflearn-spring-core-principle-advanced/advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ inflearn-spring-core-principle-advanced/advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -2,6 +2,7 @@ package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace(){
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
---
+++ inflearn-spring-core-principle-advanced/advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,42 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field(){
+        log.info("main start");
+        Runnable userA = () -> {
+          service.logic("userA");
+        };
+        Runnable userB = () ->{
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int i) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
