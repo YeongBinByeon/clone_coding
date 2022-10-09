f247a3ced55fe004b5194914f605e9933592d98a
yeongbin <yeongbin6483@gmail.com>
2022-10-09 10:11:30 +0900

    프록시 패턴과 데코레이터 패턴 카테고리 마지막 내용까지 작성하여 커밋

---
+++ inflearn-spring-core-principle-advanced/proxy-start/src/test/java/hello/proxy/pureproxy/concreateproxy/ConcreteProxyTest.java
@@ -0,0 +1,23 @@
+package hello.proxy.pureproxy.concreateproxy;
+
+import hello.proxy.pureproxy.concreateproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreateproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreateproxy.code.TimeProxy;
+import org.junit.jupiter.api.Test;
+
+public class ConcreteProxyTest {
+    @Test
+    void noProxy(){
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+
+    @Test
+    void addProxy(){
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
+    }
+}
