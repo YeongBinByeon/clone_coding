c25a7e1c4254fa206ec702b8e05ebee9a05b4597
yeongbin <yeongbin6483@gmail.com>
2023-04-11 01:16:41 +0900

    스프링부트 - 핵심 원리와 활용 강의에서 마이크로미터, 프로메테우스, 그라파나 부분 저체 커밋 및 모니터링 메트릭 활용에서 메트릭 등록1 - 카운터 까지 커밋

--- inflearn_springboot-core-principles-and-applications/actuator/src/main/java/hello/ActuatorApplication.java
+++ inflearn_springboot-core-principles-and-applications/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,9 +1,13 @@
 package hello;
 
+import hello.order.v0.OrderConfigV0;
+import hello.order.v1.OrderConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(OrderConfigV1.class)
+@SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
     public static void main(String[] args) {
