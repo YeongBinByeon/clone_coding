b8748f683de9fdb87a944ae7fc2ee76de6d2af5f
yeongbin <yeongbin6483@gmail.com>
2023-04-14 19:15:28 +0900

    모니터링 메트릭 활용 강의 내용 끝까지 커밋

--- inflearn_springboot-core-principles-and-applications/actuator/src/main/java/hello/ActuatorApplication.java
+++ inflearn_springboot-core-principles-and-applications/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,12 +1,23 @@
 package hello;
 
+import hello.order.gauge.StockConfigV1;
+import hello.order.gauge.StockConfigV2;
 import hello.order.v0.OrderConfigV0;
 import hello.order.v1.OrderConfigV1;
+import hello.order.v2.OrderConfigV2;
+import hello.order.v3.OrderConfigV3;
+import hello.order.v4.OrderConfigV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV1.class)
+//@Import(OrderConfigV0.class)
+//@Import(OrderConfigV1.class)
+//@Import(OrderConfigV2.class)
+//@Import(OrderConfigV3.class)
+//@Import(OrderConfigV4.class)
+//@Import({OrderConfigV4.class, StockConfigV1.class})
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
