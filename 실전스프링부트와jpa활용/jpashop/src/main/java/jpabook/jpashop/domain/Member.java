259fb9e1959ad2d97bc9a7e331ebbd68d168bcb7
yeongbin <yeongbin6483@gmail.com>
2023-06-05 03:49:55 +0900

    섹션2 도메인 분석 설계 전체 커밋

---
+++ 실전스프링부트와jpa활용/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY) // Order 테이블에 있는 member field
+    private List<Order> orders = new ArrayList<>();
+}
