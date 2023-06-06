3c89207429de531e5a4ee902868dd77974e5e0bb
yeongbin <yeongbin6483@gmail.com>
2023-06-06 18:00:56 +0900

    섹션4 / 회원 도메인 개발 까지 커밋

--- 실전스프링부트와jpa활용/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ 실전스프링부트와jpa활용/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -16,7 +16,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    private String username;
+    private String name;
 
     @Embedded
     private Address address;
---
+++ 실전스프링부트와jpa활용/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    //@Rollback(value = false)
+    public void 회원가입() throws Exception{
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        Assertions.assertEquals(member, memberRepository.findOne(savedId));
+
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e){
+            return;
+        }
+
+        // then
+        Assertions.fail("예외가 발생해야 한다.");
+    }
+
+}
