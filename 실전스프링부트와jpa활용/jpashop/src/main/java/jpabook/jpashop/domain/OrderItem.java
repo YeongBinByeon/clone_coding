ca19217c431151a3def1c121d847abe69b9e4005
yeongbin <yeongbin6483@gmail.com>
2023-06-09 05:32:46 +0900

    섹션6 / 주문 서비스 개발 까지 커밋

--- 실전스프링부트와jpa활용/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 실전스프링부트와jpa활용/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,13 +2,16 @@ package jpabook.jpashop.domain;
 
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -26,6 +29,7 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
 
     private int count; // 주문 수량량
+
 
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count){
