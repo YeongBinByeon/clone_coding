422d839803fa7eb08ec669d8abec705e609f1f53
yeongbin <yeongbin6483@gmail.com>
2023-06-12 03:31:24 +0900

    섹션 7 / 다음으로 (강의 끝 까지 완료) 까지 커밋

---
+++ 실전스프링부트와jpa활용/jpashop/src/test/java/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void updateTest() throws Exception{
+        Book book = em.find(Book.class, 1L);
+
+        // TX
+        book.setName("aaaa");
+    }
+}
