2b0a3f78b3b58913c7cf47a484b73721e532a8e2
yeongbin <yeongbin6483@gmail.com>
2024-09-29 20:42:47 +0900

    섹션 5 / @EntityGraph 부분 커밋

--- spring-data-jpa-practice/data-jpa/src/test/java/studu/datajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa-practice/data-jpa/src/test/java/studu/datajpa/repository/MemberRepositoryTest.java
@@ -146,4 +146,32 @@ class MemberRepositoryTest {
         Assertions.assertThat(resultCount).isEqualTo(3);
 
     }
+
+    @Test
+    public void findMemberLazy(){
+        // given
+        // member1 -> teamA
+        // member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when N + 1
+
+        List<Member> members = memberRepository.findAll();
+
+        for (Member member : members){
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+    }
 }
